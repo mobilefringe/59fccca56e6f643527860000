e07449da0b7e69960e77d106d7890b242719b862
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2017-11-03 17:32:12 -0400

    Template 1.1  /application.js

--- application.js
+++ application.js
@@ -596,7 +596,7 @@ function renderPostDetails(container, template, collection, blog_posts){
             }
         }
 
-        val.twitter_title = val.title + " via @DevonshireMall";
+        val.twitter_title = val.title + " via @TwinPinesMall"; //Change to Mall Name
         
         var rendered = Mustache.render(template_html,val);
         item_rendered.push(rendered);
